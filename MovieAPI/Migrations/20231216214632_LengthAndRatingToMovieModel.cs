2d3c31f908fe2bc52ad851a79649ba0047ee0133
Jason E. Church <elijahchurch@gmail.com>
2023-12-16 13:48:17 -0800

    Create Length and Rating fields for Movie model. Create migration

---
+++ MovieAPI/Migrations/20231216214632_LengthAndRatingToMovieModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    public partial class LengthAndRatingToMovieModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Length",
+                table: "Movies",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movies");
+        }
+    }
+}
