3b44b65f60301b09fe320f4386d76f845c25b8ef
Jason E. Church <elijahchurch@gmail.com>
2024-01-16 17:47:47 -0800

    Create movie-entry form. WIP: Style

--- movie-client/src/components/MovieForm.tsx
+++ movie-client/src/components/MovieForm.tsx
@@ -1,7 +1,26 @@
 const MovieForm = () => {
     return(
         <div>
-            <h1>Movie Form</h1>
+            <form className="formContainer">
+                <h1> Movie Entry</h1>
+                <label htmlFor="movieName">Name</label>
+                <input id="movieName" name="movieName" type="text"></input>
+                <label htmlFor="movieYear">Release Year</label>
+                <input id="movieYear" type="number" min="1888" max="2024" name="movieYear"></input>
+                <label htmlFor="rating">Rating</label>
+                <select name="rating">
+                    <option value="G">G</option>
+                    <option value="PG">PG</option>
+                    <option value="PG-13">PG-13</option>
+                    <option value="R">R</option>
+                    <option value="NR">NR</option>
+                </select>
+                <label htmlFor="movieLength"> Run Time</label>
+                <input name="movieLength" type="number" min="0"></input>
+                <label htmlFor="overView">Synopsis</label>
+                <textarea></textarea>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
