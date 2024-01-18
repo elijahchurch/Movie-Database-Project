550010ab44203b71a19aa5bf43db333e0e065518
Jason E. Church <elijahchurch@gmail.com>
2024-01-18 14:44:53 -0800

    Add link to home page in drop down menu

--- movie-client/src/components/Header.tsx
+++ movie-client/src/components/Header.tsx
@@ -10,10 +10,11 @@ const Header = () => {
 
     return (
         <div id="navBar">
-            <h1 id="headerTitle">Title of Movie App</h1>
+            <h1 id="headerTitle">Movie App Home</h1>
             <div className="dropDown">
                 <button onClick={handleDropDownClick} id="menuButton"><img id="menuImage" src={dropDownIsClicked ? "img/icons/x.png":"img/icons/menu.png"} alt="menu icon"/></button>
                 <div className={dropDownIsClicked ? "dropDown-options" : "dropDown-options hidden"}>
+                    <Link onClick={handleDropDownClick} className="dropDown-links" to="/">Home</Link>
                     <Link onClick={handleDropDownClick} className="dropDown-links" to="/movieForm">Add Movie</Link>
                 </div>
             </div>
