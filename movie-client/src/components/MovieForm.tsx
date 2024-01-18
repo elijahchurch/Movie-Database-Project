bf6d6a88b6c522944c66842fd1ee686362aa2d37
Jason E. Church <elijahchurch@gmail.com>
2024-01-18 15:32:03 -0800

    Create handleChange event for movie form

--- movie-client/src/components/MovieForm.tsx
+++ movie-client/src/components/MovieForm.tsx
@@ -1,14 +1,57 @@
+import React, {MouseEvent, ChangeEvent, useState} from "react";
+
 const MovieForm = () => {
+    const [movieName, setMovieName] = useState("");
+    const [movieYear, setMovieYear] = useState("");
+    const [rating, setRating] = useState("");
+    const [movieLength, setMovieLength] = useState("");
+    const [overView, setOverView] = useState("");
+
+    const handleChange = (event : React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        event.preventDefault;
+        switch (event.currentTarget.name) {
+            case "movieName":
+                setMovieName(event.currentTarget.value);
+                break;
+            case "movieYear":
+                setMovieYear(event.currentTarget.value);
+                break;
+            case "rating":
+                setRating(event.currentTarget.value);
+                break;
+            case "movieLength":
+                setMovieLength(event.currentTarget.value);
+                break;
+            case "overView":
+                setOverView(event.currentTarget.value);
+                break;
+        }
+    }
+
+    const handleMovieForm = async (event : MouseEvent) => {
+        event?.preventDefault();
+        console.log(event);
+    }
     return(
         <div>
             <form className="formContainer">
-                <h1> Movie Entry</h1>
+                <h2> Movie Entry</h2>
                 <label htmlFor="movieName">Name</label>
-                <input id="movieName" name="movieName" type="text"></input>
+                <input 
+                    name="movieName"
+                    type="text"
+                    onChange={handleChange}
+                />
                 <label htmlFor="movieYear">Release Year</label>
-                <input id="movieYear" type="number" min="1888" max="2024" name="movieYear"></input>
+                <input 
+                    name="movieYear"
+                    type="number"
+                    min="1888"
+                    max="2024"
+                    onChange={handleChange}
+                />
                 <label htmlFor="rating">Rating</label>
-                <select name="rating">
+                <select name="rating" onChange={handleChange}>
                     <option value="G">G</option>
                     <option value="PG">PG</option>
                     <option value="PG-13">PG-13</option>
@@ -16,10 +59,15 @@ const MovieForm = () => {
                     <option value="NR">NR</option>
                 </select>
                 <label htmlFor="movieLength"> Run Time</label>
-                <input name="movieLength" type="number" min="0"></input>
+                <input 
+                    name="movieLength"
+                    type="number"
+                    min="0"
+                    onChange={handleChange}
+                />
                 <label htmlFor="overView">Synopsis</label>
-                <textarea></textarea>
-                <button type="submit">Submit</button>
+                <textarea name="overView" onChange={handleChange}></textarea>
+                <button onClick={handleMovieForm} type="submit">Submit</button>
             </form>
         </div>
     )
