de2b144decf54c6ac505a97176c628a6fde3449c
Jason E. Church <elijahchurch@gmail.com>
2024-01-18 16:45:02 -0800

    Change Rating column in Movie model to string from Enum and update migrations

--- MovieAPI/Models/Movie.cs
+++ MovieAPI/Models/Movie.cs
@@ -1,21 +1,12 @@
 namespace MovieApi.Models
 {
-    public enum MovieRating 
-    {
-        G,
-        PG,
-        PG13,
-        R,
-        NC17,
-        NR,
-    }
     public class Movie
     {
         public int MovieId {get; set;}
         public string Name { get; set;}
         public int Date { get; set;}
         public int Length {get; set;}
-        public MovieRating Rating { get; set;}
+        public string Rating { get; set;}
         public string OverView { get; set;}
         #nullable enable
         public Poster? Poster { get; set;}
